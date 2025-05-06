2d4f8764935b8b6487dcf8aace730e1a9119dc34
Yang Songxiao <yangsongxiao0616@gmail.com>
2025-05-06 23:35:59 +0900

    Cleaning

--- test.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-ckpt = torch.load('/mnt/data1/songxiao/CarpalData/ckpts/Baseline_v1_unet_202504140442/model_best.pth', map_location='cpu')
-for k in ckpt['model'].keys():
-    print(k)
